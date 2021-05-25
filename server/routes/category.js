0d39853aeb8802de54382c7601c15b98e70f861b
Ubuntu <rlahyuna1234@gmail.com>
2021-05-25 15:24:34 +0000

    implement coin api

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,6 +1,13 @@
+var axios = require("axios");
 var express = require('express');
 var router = express.Router();
+
+let request = require('request');
+let cheerio = require('cheerio');
+require('dotenv').config({ path: "../.env" });
+
 const { PythonShell } = require("python-shell");
+
 
 /* GET weather */
 router.get('/weather/:location', function(req, res, next) {
@@ -54,11 +61,65 @@ router.get('/stock/:tickernumber', function(req, res, next) {
 });
 
 
+/* GET coin */
+router.get('/coin/:symbol', function(req, res, next) {
 
-// /* GET bus */
-// router.get('/bus/:buscode', function(req, res, next) {
-//
-// });
+    let symbol = req.params.symbol;
 
+    axios.get('https://api.bithumb.com/public/ticker/'+symbol)
+        .then(function (response) {
+            return res.json(response.data);
+        })
+        .catch(function (error) {
+            return res.json("cannot load bithumb data");
+        })
+
+
+
+});
+
+
+// 도착 정보 얻어옴
+function getArriveData(routeId, arsId, direction) {
+    // new Promise() 추가
+    return new Promise(function(resolve, reject) {
+
+        var defaultUrl = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?serviceKey="+process.env.OpenAPIKey+"&arsId="+arsId;
+
+
+        console.log(defaultUrl);
+
+        request(defaultUrl, function(err, res, body){
+            var $=cheerio.load(body)
+
+            // 각각 버스 도착 정보는 busArrivalList 태그 안에 들어있음
+            // 따라서 each문 이용
+            $("itemList").each(function(index, bus){
+                // bus 변수를 context로 주어서 선택자 이용
+
+                if($('busRouteId', bus).text() == routeId && $('adirection', bus).text() == direction) {
+                    arriveResult = {"firstArr" : $('arrmsg1', bus).text(), "secondArr" : $('arrmsg2', bus).text()};
+                    resolve(arriveResult);
+                }
+            })
+        })
+    });
+}
+
+// 버스 routeID, 정류장 고유 번호, 버스 방향 필요
+/* GET bus */
+router.get('/bus/:busrouteid/:busstoparsid/:direction', function(req, res, next) {
+
+    const busrouteid = req.params.busrouteid; //노선
+    const busstoparsid = req.params.busstoparsid; //버스 고유 정거장
+    const direction = req.params.direction; //방향
+
+
+    getArriveData(busrouteid, busstoparsid, direction)
+        .then(function(arriveResult){
+            return res.json(arriveResult);
+        })
+
+});
 
 module.exports = router;
