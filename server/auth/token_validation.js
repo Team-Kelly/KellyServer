10448abcc9d248457c62294525366087d2339e3b
kha-github <rlahyuna1234@gmail.com>
2021-07-02 00:56:23 +0900

    jwt refresh token

--- server/auth/token_validation.js
+++ server/auth/token_validation.js
@@ -1,26 +1,35 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-    checkToken: (req, res, next) => {
+    checkAccessToken: (req, res, next) => {
         let token = req.get("authorization");
         if (token) {
             token = token.slice(7);
             jwt.verify(token, ""+process.env.JWT_Secret_Key, (err, decoded) => {
                 if (err) {
-                    return res.json({
-                        success: 0,
-                        message: "Invalid Token..."
-                    });
+                    return res.sendStatus(403); //invalid token
                 } else {
                     req.decoded = decoded;
                     next();
                 }
             });
         } else {
-            return res.json({
-                success: 0,
-                message: "Access Denied! Unauthorized User"
+            return res.sendStatus(400); //Access Denied, worng access token or not sended
+        }
+    },
+    checkRefreshToken: (req, res, next) => {
+        let token = req.body.refreshtoken;
+        if (token) {
+            jwt.verify(token, ""+process.env.JWT_Refresh_Key, (err, decoded) => {
+                if (err) {
+                    return res.sendStatus(403); //invalid token
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
             });
+        } else {
+            return res.sendStatus(401); //Access Denied, worng refresh token or not sended
         }
     }
 };
--- server/routes/user/user_router.js
+++ server/routes/user/user_router.js
@@ -1,11 +1,12 @@
-const { createUser, getUserByUserID, getUsers, updateUser, login } = require("./user_controller");
+const { createUser, getUserByUserID, getUsers, updateUser, login, refresh } = require("./user_controller");
 const router = require("express").Router();
-const { checkToken } = require("../../auth/token_validation");
+const { checkAccessToken, checkRefreshToken } = require("../../auth/token_validation");
 
 router.post("/join", createUser);
-router.get("/alluser", checkToken, getUsers);
-router.get("/:id", checkToken, getUserByUserID);
-router.patch("/update", checkToken, updateUser);
+router.get("/alluser", checkAccessToken, getUsers);
+router.get("/:id", checkAccessToken, getUserByUserID);
+router.patch("/update", checkAccessToken, updateUser);
 router.post("/login", login);
+router.post("/refresh", checkRefreshToken, refresh);
 
 module.exports = router;
